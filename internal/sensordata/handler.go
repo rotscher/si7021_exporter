d78794b319236b87dd9eae0f03d08d879b98a48a
Roger Brechbühl <rotscher.ch@gmail.com>
2021-12-21 10:10:54 +0100

    Fix exporter

--- internal/sensordata/handler.go
+++ internal/sensordata/handler.go
@@ -10,13 +10,12 @@ import (
 var errorCnt int32
 
 func (sd *SensorData) Export(w http.ResponseWriter, _ *http.Request) {
-	t, rh, err := sd.ReadRelativeHumidityAndTemperature()
+	rh, t, err := sd.ReadRelativeHumidityAndTemperature()
 	if err != nil {
 		log.Print(err)
 		_, _ = fmt.Fprintf(w, "si7021_error %d", atomic.AddInt32(&errorCnt, 1))
 		return
 	}
-	_, _ = fmt.Fprintf(w, "si7021_temperature %f", t)
-	_, _ = fmt.Fprintf(w, "si7021_relative_humidity %f", rh)
-	w.WriteHeader(200)
+	_, _ = fmt.Fprintf(w, "si7021_temperature %f\n", t)
+	_, _ = fmt.Fprintf(w, "si7021_relative_humidity %f\n", rh)
 }
